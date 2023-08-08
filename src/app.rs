7f3eb3cf0eec90d8ca0cb4d8d931c2bdc1986d7c
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2023-08-08 19:55:25 +0200

    feat(common): adjust heading

--- src/app.rs
+++ src/app.rs
@@ -32,7 +32,7 @@ fn HomePage(cx: Scope) -> impl IntoView {
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! { cx,
-        <h1>"AoC CAU"</h1>
+        <h1>"AoC CAU Discord"</h1>
         <button on:click=on_click>"Click Me: " {count}</button>
     }
 }
